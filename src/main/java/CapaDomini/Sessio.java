b2cf2026a8a9047b1e9bb903367a3f88098122c4
David Pina <dpina@edu.tecnocampus.cat>
2019-05-09 13:29:31 +0200

    Constructor sessió 0.1

--- src/main/java/CapaDomini/Sessio.java
+++ src/main/java/CapaDomini/Sessio.java
@@ -1,15 +1,55 @@
 package CapaDomini;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Sessio {
 
 	private String strUsuariNom;
 	private Timer timInactivitat;
 	private int iNumPartides;
+	private int iNumPendents; // Partides pendents de la teva atenció
 	private boolean boolConnectat;
+	private TimerTask task;
 	
-	public Sessio() {
+	/**
+	 * Construeix una sessió, possant el timer a 10 minuts del time-out
+	 * en el cas de tenir moviments pendents, 20 minuts altrament.
+	 * @param strNom Nom de l'usuari que utilitza la sessió
+	 * @param iPartides Número de partides que té l'usuari
+	 * @param iPendents Número de partides pendents de l'atenció de l'usuari
+	 * @throws IllegalArgumentException Nom no pot ser null, ni tenir menys de tres caracters. Partides no pot ser menor que zero.
+	 */
+	public Sessio(String strNom, int iPartides, int iPendents) throws IllegalArgumentException {
+		/* Comprovació d'arguments */
+		if (strNom == null) {
+			throw new IllegalArgumentException("El nom no pot ser null.");
+		}
+		if (strNom.length() < 3) {
+			throw new IllegalArgumentException("El nom no pot tenir menys de tres caracters.");
+		}
+		if (iPartides < 0) {
+			throw new IllegalArgumentException("L'usuari no pot tenir menys de 0 partides.");
+		}
+		if (iPendents < 0) {
+			throw new IllegalArgumentException("Les partides pendents no poden ser menys de 0.");
+		}
 		
+		/* Assignació d'atributs */
+		this.strUsuariNom = strNom;
+		this.iNumPartides = iPartides;
+		this.iNumPendents = iPendents;
+		
+		/* Configurem el timer */
+		this.timInactivitat = new Timer();
+		this.task = new TimerTask() {
+	        @Override
+	        public void run()
+	        {
+	            timeOut();
+	        }
+		};
+		this.timInactivitat.schedule(task, 10*1000*60);
 	}
 	
 	public void timeOut() {
@@ -18,6 +58,7 @@ public class Sessio {
 	
 	public boolean registrarUsuari() {
 		
+		return false;
 	}
 	
 	public List<String> veureUsuaris() {
