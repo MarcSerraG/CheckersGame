6b0d117ba852644d401d25bbce58be39a263f94f
David Pina <dpina@edu.tecnocampus.cat>
2019-05-12 17:54:00 +0200

    Partida: getters, equals i hashCode

--- src/main/java/CapaDomini/Partida.java
+++ src/main/java/CapaDomini/Partida.java
@@ -11,6 +11,29 @@ public class Partida {
 		
 	}
 	
+	public String getID() {return this.strID;}
+	public String getContrincant() {return this.strContrincant;}
+	public String getUsuariTorn() {return this.strUsuariTorn;}
+	public boolean getPartidaEnCurs() {return this.boolPartidaEnCurs;}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (o == this) {
+			return true;
+		}
+		
+		if (!(o instanceof Partida)) {
+			return false;
+		}
+		
+		return this.strID.equals(((Partida)o).strID);
+	}
+	
+	@Override
+	public int hashCode() {
+		return this.strID.hashCode();
+	}
+	
 	public boolean comprovarTorn() {
 		
 		return true;
