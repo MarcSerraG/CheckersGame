6f943c513fa7fe5721027423f2d975e3174ecf2a
David Pina <dpina@edu.tecnocampus.cat>
2019-05-18 13:37:18 +0200

    JocAPI: constructor, login temporal, password hashing

---
+++ src/main/java/CapaAPI/JocAPI.java
@@ -0,0 +1,45 @@
+package CapaAPI;
+
+import CapaPersistencia.*;
+import CapaDomini.*;
+import org.json.JSONObject;
+import com.lambdaworks.crypto.SCryptUtil;
+
+public class JocAPI {
+	
+	private ConnectionSQLOracle connSQL;
+	private UsuariSQLOracle userSQL;
+	private PartidesSQLOracle partSQL;
+	
+	public JocAPI() throws Exception {
+		connSQL = new ConnectionSQLOracle("dpina", "dpina");
+		userSQL = new UsuariSQLOracle(connSQL);
+		partSQL = new PartidesSQLOracle(connSQL);
+	}
+
+	/**
+	 * Comprova les dades de l'usuari passat per parametres, i fa login si son correctes
+	 * @param user nom de l'usuari
+	 * @param password contrasenya de l'usuari, SENSE hashing
+	 * @return "res": "IdSessio", "err": "usuari o contrasenya incorrecte", "sErr": "";
+	 */
+	public String login(String user, String password) {
+		
+		JSONObject json = new JSONObject();
+		String BDPassword;
+		boolean passwordMatch;
+		json.put("res", "temporalID");
+		json.put("err", "");
+		json.put("sErr", "");
+		
+		/* Password checking */
+		BDPassword = this.userSQL.getPasword(user);
+		if (BDPassword == null) json.put("err", "usuari o contrasenya incorrecte");
+		else {
+			passwordMatch = SCryptUtil.check(password, BDPassword);
+			if (!passwordMatch) json.put("err", "usuari o contrasenya incorrecte");
+		}
+		
+		return json.toString();
+	}
+}
