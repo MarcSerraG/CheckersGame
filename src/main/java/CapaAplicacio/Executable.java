32caed714d29ec09efef310d3eb871a337af4d17
Ricard <rzambranoi@edu.tecnpocampus.cat>
2019-05-23 08:35:43 +0200

    Solucio botons Mac 2

--- src/main/java/CapaAplicacio/Executable.java
+++ src/main/java/CapaAplicacio/Executable.java
@@ -4,6 +4,8 @@ import java.awt.EventQueue;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
+import javax.swing.UIManager;
+
 public class Executable {
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -15,7 +17,9 @@ public class Executable {
 					frame.setSize(1200, 700);
 					frame.setLocationRelativeTo(null);
 					frame.setTitle("Joc de Dames");
+					UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
 					frame.addWindowListener(new WindowAdapter() {
+
 						@Override
 						public void windowClosing(WindowEvent event) {
 							frame.CloseConnection();
