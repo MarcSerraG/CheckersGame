71fcc46c0906e08c2b474d7e32b34cedb51d968a
David Pina <dpina@edu.tecnocampus.cat>
2019-06-03 18:01:42 +0200

    API ferBufa

--- src/main/java/CapaAplicacio/JocAPI.java
+++ src/main/java/CapaAplicacio/JocAPI.java
@@ -379,22 +379,19 @@ public class JocAPI {
 
 	public String ferBufa(String idSessio, String idPartida, String pos) {
 
-		String estatTauler = this.partSQL.continuarPartida(idPartida);
-		if (estatTauler == null)
-			return crearJSON("", "No s'ha pogut carregar la partida", "");
-
-		Taulell tauler = new Taulell();
-		tauler.reconstruirTaulell(estatTauler);
-
+		this.instanciarMoviments(idPartida);
+		
 		int xIni = Integer.parseInt(pos.split(";")[0]);
 		int yIni = Integer.parseInt(pos.split(";")[1]);
-
-		Casella cas = tauler.seleccionarCasella(xIni, yIni);
-		/*if (tauler.bufar(cas))
-			return crearJSON("true", "", "");
+		
+		boolean bufa = this.movTornAct.ferBufa(xIni, yIni);
+		String res;
+		if (bufa)
+			res = "true";
 		else
-			return crearJSON("false", "", "");*/
-		return crearJSON("res","","");
+			res = "false";
+		
+		return crearJSON(res, "", "");
 	}
 
 	public String acceptaTaules(String idSessio, String idPartida) {
