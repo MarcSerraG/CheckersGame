18914812dbd1f8e5cc37c95739b5c6c85a9c9c7e
Ricard <rzambranoi@edu.tecnpocampus.cat>
2019-06-13 12:40:42 +0200

    Imatges dames i peons
    S'ha millorat i simplificat la carrega de imatges per tal que no es carreguin cada vegada que es carrega un taulell, només quan iniciem una partida

--- src/main/java/CapaPresentacio/Partida.java
+++ src/main/java/CapaPresentacio/Partida.java
@@ -8,7 +8,6 @@ import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -34,6 +33,7 @@ public class Partida extends JPanel implements ActionListener {
 	String posInicial = "", posFinal = "", idPartida, NomContrincant, ContrincantColor, JugadorColor;
 	Boolean torn;
 	private int blanques, negres;
+	private ImageIcon peoNegre, peoBlanca, DamaBlanca, DamaNegra;
 
 	public Partida(BaseInterficie base, String contrincant, Boolean torn) {
 		interficieBase = base;
@@ -50,6 +50,11 @@ public class Partida extends JPanel implements ActionListener {
 	public JPanel partidaCreate(String idPartida, boolean res, String taulellAntic) {
 		panelTaulell = new JPanel();
 		this.idPartida = idPartida;
+
+		peoNegre = new ImageIcon(cargaImatge("/PeoNegre.png"));
+		peoBlanca = new ImageIcon(cargaImatge("/PeoBlanca.png"));
+		DamaBlanca = new ImageIcon(cargaImatge("/DamaBlanca.png"));
+		DamaNegra = new ImageIcon(cargaImatge("/DamaNegra.png"));
 
 		panelTaulell.setLayout(new BorderLayout());
 
@@ -78,6 +83,17 @@ public class Partida extends JPanel implements ActionListener {
 		return panelTaulell;
 	}
 
+	private Image cargaImatge(String path) {
+		try {
+			Image myImage = ImageIO.read(getClass().getResource(path));
+			myImage = myImage.getScaledInstance(35, 35, java.awt.Image.SCALE_SMOOTH);
+			return myImage;
+		} catch (Exception e) {
+			System.out.print("Error en cargar la imagen " + e);
+		}
+		return null;
+	}
+
 	private void setAnticTaulell(String taulellSQL) {
 
 		if (panelCentral != null) {
@@ -102,77 +118,55 @@ public class Partida extends JPanel implements ActionListener {
 
 		String[] divisioTaulell = taulellSQL.split("[,\n]");
 
-		try {
-
-			Image myImage = ImageIO.read(getClass().getResource("/PeoNegre.png"));
-			myImage = myImage.getScaledInstance(35, 35, java.awt.Image.SCALE_SMOOTH);
-			ImageIcon peoNegre = new ImageIcon(myImage);
-
-			Image myImage2 = ImageIO.read(getClass().getResource("/PeoBlanca.png"));
-			myImage2 = myImage2.getScaledInstance(35, 35, java.awt.Image.SCALE_SMOOTH);
-			ImageIcon peoBlanca = new ImageIcon(myImage2);
-
-			Image myImage3 = ImageIO.read(getClass().getResource("/DamaBlanca.png"));
-			myImage3 = myImage3.getScaledInstance(35, 35, java.awt.Image.SCALE_SMOOTH);
-			ImageIcon DamaBlanca = new ImageIcon(myImage3);
-
-			Image myImage4 = ImageIO.read(getClass().getResource("/DamaNegra.png"));
-			myImage4 = myImage4.getScaledInstance(35, 35, java.awt.Image.SCALE_SMOOTH);
-			ImageIcon DamaNegra = new ImageIcon(myImage4);
-
-			for (int x = 0; x < 8; x++) {
-				switch (color) {
-				case -1:
-				case 0:
-					color++;
-					break;
-				case 1:
-					color--;
-					break;
-				}
-
-				for (int y = 0; y < 8; y++) {
-					switch (divisioTaulell[contador]) {
-					case "1":
-						taulell2.put(createButton(size, color, peoNegre, ContrincantPesses("Black")), x + ";" + y);
-						negres++;
-						break;
-					case "0":
-						taulell2.put(createButton(size, color, peoBlanca, ContrincantPesses("Red")), x + ";" + y);
-						blanques++;
-						break;
-					case "d":
-						taulell2.put(createButton(size, color, DamaNegra, ContrincantPesses("Black")), x + ";" + y);
-						negres++;
-						break;
-					case "D":
-						taulell2.put(createButton(size, color, DamaBlanca, ContrincantPesses("Red")), x + ";" + y);
-						blanques++;
-						break;
-					default:
-						taulell2.put(createButton(size, color, null, true), x + ";" + y);
-						break;
-					}
-
-					contador++;
-
-					if (color == 0)
-						color++;
-					else
-						color--;
-				}
+		for (int x = 0; x < 8; x++) {
+			switch (color) {
+			case -1:
+			case 0:
+				color++;
+				break;
+			case 1:
+				color--;
+				break;
 			}
 
-			for (JButton b : taulell2.keySet())
-				panelCentral.add(b);
+			for (int y = 0; y < 8; y++) {
+				switch (divisioTaulell[contador]) {
+				case "1":
+					taulell2.put(createButton(size, color, peoNegre, ContrincantPesses("Black")), x + ";" + y);
+					negres++;
+					break;
+				case "0":
+					taulell2.put(createButton(size, color, peoBlanca, ContrincantPesses("Red")), x + ";" + y);
+					blanques++;
+					break;
+				case "d":
+					taulell2.put(createButton(size, color, DamaNegra, ContrincantPesses("Black")), x + ";" + y);
+					negres++;
+					break;
+				case "D":
+					taulell2.put(createButton(size, color, DamaBlanca, ContrincantPesses("Red")), x + ";" + y);
+					blanques++;
+					break;
+				default:
+					taulell2.put(createButton(size, color, null, true), x + ";" + y);
+					break;
+				}
 
-			panelTaulell.add(panelCentral, BorderLayout.CENTER);
-			panelCentral.setVisible(true);
-			validate();
+				contador++;
 
-		} catch (IOException e) {
-			System.out.println("setAnticTaulell Error: " + e);
+				if (color == 0)
+					color++;
+				else
+					color--;
+			}
 		}
+
+		for (JButton b : taulell2.keySet())
+			panelCentral.add(b);
+
+		panelTaulell.add(panelCentral, BorderLayout.CENTER);
+		panelCentral.setVisible(true);
+		validate();
 
 	}
 
@@ -432,6 +426,10 @@ public class Partida extends JPanel implements ActionListener {
 		} else {
 			lMessage.setText(err);
 		}
+	}
+
+	private void Bufar() {
+
 	}
 
 }
